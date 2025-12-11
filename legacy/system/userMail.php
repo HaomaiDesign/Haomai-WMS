eb18484fa9ddb3bccf57e580431a592cddcf8839
Nico <31294589+NicoGarofalo@users.noreply.github.com>
2025-12-11 19:11:27 -0300

    removing keys

--- legacy/system/userMail.php
+++ legacy/system/userMail.php
@@ -10,12 +10,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\n    \"personalizations\": [\n        {\n            \"to\": [\n                {\n                    \"email\": \"".$haomaiEmail."\"\n                }\n            ]\n        }\n    ],\n    \"from\": {\n        \"email\": \"info@haomai.com.ar\"\n    },\n    \"subject\": \"Nuevo Usuario Registrado \",\n    \"content\": [\n        {\n            \"type\": \"text/plain\",\n            \"value\": \"Nuevo usuario registrado en Haomai Market ".$fullName." / ".$email."\"\n        }\n    ]\n}",
-  CURLOPT_HTTPHEADER => array(
-    "Authorization: Bearer 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",
-    "Content-Type: application/json",
-    "cache-control: no-cache"
-  ),
+  CURLOPT_POSTFIELDS => "{\n    \"personalizations\": [\n        {\n            \"to\": [\n                {\n                    \"email\": \"" . $haomaiEmail . "\"\n                }\n            ]\n        }\n    ],\n    \"from\": {\n        \"email\": \"info@haomai.com.ar\"\n    },\n    \"subject\": \"Nuevo Usuario Registrado \",\n    \"content\": [\n        {\n            \"type\": \"text/plain\",\n            \"value\": \"Nuevo usuario registrado en Haomai Market " . $fullName . " / " . $email . "\"\n        }\n    ]\n}",
 ));
 
 $response = curl_exec($curl);
@@ -40,13 +35,13 @@ $ch = curl_init($url);
 $data = '{"personalizations": [{"to": [{"email": "andres.wei@hotmail.com"}]}],"from": {"email": "info@haomai.com.ar"},"subject": "Sending with SendGrid is Fun","content": [{"type": "text/plain", "value": "and easy to do anywhere, even with cURL"}]}';
 
 $data = array ( 'personalizations' => array ( 
-									'o' => array ('email' => 'andres.wei@hotmail.com') ),
-				'from' => array ('email' => 'info@haomai.com.ar'),
-				'subject' => 'Nuevo Pedido', 
-				'content' => array ( 
-							'type' => 'text/plain', 
-							'value' => 'Pedido Pedido' ));
-							
+                  'o' => array ('email' => 'andres.wei@hotmail.com') ),
+        'from' => array ('email' => 'info@haomai.com.ar'),
+        'subject' => 'Nuevo Pedido', 
+        'content' => array ( 
+              'type' => 'text/plain', 
+              'value' => 'Pedido Pedido' ));
+
 $command = json_encode($data);
 
 //attach encoded JSON string to the POST fields
@@ -66,15 +61,15 @@ $err = curl_error($ch);
 if ($err) {
   echo "cURL Error #:" . $err;
 } else {
-	$result = json_decode($exec);
-	
-	if (isset($result->message))
-	{
-		echo $result->message; 
-	}
-	
-		echo $data;
-	}
+  $result = json_decode($exec);
+
+  if (isset($result->message))
+  {
+    echo $result->message; 
+  }
+
+    echo $data;
+  }
 
 
 
--- legacy/users/userProfileUpdate.php
+++ legacy/users/userProfileUpdate.php
@@ -4,165 +4,157 @@ include '../system/session.php';
 include '../system/formQuery.php';
 require '../vendor/autoload.php';
 
-$IAM_KEY = 'AKIATDKHWMZIDQDCJ6F6';
-$IAM_SECRET = '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';
-
 use Aws\S3\S3Client;
 use Aws\S3\Exception\S3Exception;
 
-$_SESSION['userLog']['module']="My Profile";
+$_SESSION['userLog']['module'] = "My Profile";
 
-if (($_GET['formStatus']=='create')or ($_GET['formStatus']=='edit'))
-{
+if (($_GET['formStatus'] == 'create') or ($_GET['formStatus'] == 'edit')) {
 
-// if ($_GET['formStatus']=='create')
+	// if ($_GET['formStatus']=='create')
 // 	$stmt = mysqli_query( $conn, $sql);  
 // else
 // 	$stmt = mysqli_multi_query( $conn, $sql);  	
 
-$stmt = mysqli_query( $conn, $sql); 
-  
-if ( $stmt ) {
+	$stmt = mysqli_query($conn, $sql);
 
-	$bucket = 'asiaorientalbucket';
-	
-	$s3 = S3Client::factory(array(
-		'key' => $IAM_KEY, 
-		'secret' => $IAM_SECRET, 
-		'region' => 'sa-east-1',
-		'version' => 'latest',
-		'signature' => 'v4'
-	));
+	if ($stmt) {
 
+		$bucket = 'asiaorientalbucket';
 
-	if ($_GET['formStatus']=='edit')
-	{	
-		if ($_POST['languageId']!=""){
-			unset($_SESSION['language']);
-			$_SESSION['user']['languageId'] = $_POST['languageId'];	
-			include '../system/languageSettings.php';
-		}
-
-		
-		if (isset($_POST['email']))
-			$_SESSION['user']['email'] = $_POST['email'];
-		if (isset($_POST['fullName']))
-			$_SESSION['user']['fullName'] = $_POST['fullName'];
-		if (isset($_POST['username']))	
-			$_SESSION['user']['username'] = $_POST['username'];
-		
-		$_SESSION['notification']['type']="success";
-		$_SESSION['notification']['message']=$_SESSION['language']['Your profile is updated properly'];
-		$_SESSION['userLog']['description']="The user profile is updated properly.";	
-
-		if ($_SESSION["form"]["imageFile"]!=""){
-		
-		
-		
-		
-		////////////////
-		
-		if($_FILES['avatar']['type']=='image/gif') {
-			$file_name = "A".$_SESSION['user']['id'].".gif";
-			$type = "image/gif";
-		};
-		if($_FILES['avatar']['type']=='image/png') {
-			$file_name = "A".$_SESSION['user']['id'].".png";
-			$type = "image/png";
-		};
-		if($_FILES['avatar']['type']=='image/jpeg') {
-			$file_name = "A".$_SESSION['user']['id'].".jpeg";
-			$type = "image/jpeg";
-		};
-		
-		$_SESSION["form"]["imageFile"] = $_SESSION["form"]["imageDir"].$_SESSION["form"]["imageFile"];	
-		mkdir($_SESSION["form"]["imageDir"], 0777);		
-		
-		$flagUploaded = false;
-		
-		$max_ancho = 800;
-		$max_alto = 600;
-		
-		$medidasimagen= getimagesize($_FILES['avatar']['tmp_name']);
-	
-	
-		//Si las imagenes tienen una resoluciÃ³n y un peso aceptable se suben tal cual
-			if($medidasimagen[0] < 1280 && $_FILES['avatar']['size'] < 1000000){
-
-			$nombrearchivo=$_FILES['avatar']['name'];
-			move_uploaded_file($_FILES["avatar"]["tmp_name"], $_SESSION["form"]["imageFile"]);
-			$flagUploaded = true;
-		}
+		$s3 = S3Client::factory(array(
+			'key' => $IAM_KEY,
+			'secret' => $IAM_SECRET,
+			'region' => 'sa-east-1',
+			'version' => 'latest',
+			'signature' => 'v4'
+		));
 
 
-		//Si no, se generan nuevas imagenes optimizadas
-		else {
-		$nombrearchivo=$_FILES['avatar']['name'];
+		if ($_GET['formStatus'] == 'edit') {
+			if ($_POST['languageId'] != "") {
+				unset($_SESSION['language']);
+				$_SESSION['user']['languageId'] = $_POST['languageId'];
+				include '../system/languageSettings.php';
+			}
 
-		//Redimensionar
-		$rtOriginal=$_FILES['avatar']['tmp_name'];
 
-		if($_FILES['avatar']['type']=='image/jpeg'){
-		$original = imagecreatefromjpeg($rtOriginal);
-		}
-		else if($_FILES['avatar']['type']=='image/png'){
-		$original = imagecreatefrompng($rtOriginal);
-		}
-		else if($_FILES['avatar']['type']=='image/gif'){
-		$original = imagecreatefromgif($rtOriginal);
-		}
+			if (isset($_POST['email']))
+				$_SESSION['user']['email'] = $_POST['email'];
+			if (isset($_POST['fullName']))
+				$_SESSION['user']['fullName'] = $_POST['fullName'];
+			if (isset($_POST['username']))
+				$_SESSION['user']['username'] = $_POST['username'];
 
-		list($ancho,$alto)=getimagesize($rtOriginal);
+			$_SESSION['notification']['type'] = "success";
+			$_SESSION['notification']['message'] = $_SESSION['language']['Your profile is updated properly'];
+			$_SESSION['userLog']['description'] = "The user profile is updated properly.";
 
-		$x_ratio = $max_ancho / $ancho;
-		$y_ratio = $max_alto / $alto;
-		
-		if( ($ancho <= $max_ancho) && ($alto <= $max_alto) ){
-			$ancho_final = $ancho;
-			$alto_final = $alto;
-		}
-		elseif (($x_ratio * $alto) < $max_alto){
-			$alto_final = ceil($x_ratio * $alto);
-			$ancho_final = $max_ancho;
-		}
-		else{
-			$ancho_final = ceil($y_ratio * $ancho);
-			$alto_final = $max_alto;
-		}
+			if ($_SESSION["form"]["imageFile"] != "") {
 
-		$lienzo=imagecreatetruecolor($ancho_final,$alto_final); 
 
-		imagecopyresampled($lienzo,$original,0,0,0,0,$ancho_final, $alto_final,$ancho,$alto);
-		 
-		//imagedestroy($original);
-		 
-		if($_FILES['avatar']['type']=='image/jpeg'){
-		imagejpeg($lienzo, $_SESSION["form"]["imageFile"]);
-		$flagUploaded = true;
-		}
-		else if($_FILES['avatar']['type']=='image/png'){
-		imagepng($lienzo, $_SESSION["form"]["imageFile"]);
-		$flagUploaded = true;
-		}
-		else if($_FILES['avatar']['type']=='image/gif'){
-		imagegif($lienzo, $_SESSION["form"]["imageFile"]);
-		$flagUploaded = true;
-		}
-		
-		
-		}
-		////////
-		
 
-				
-				if ($flagUploaded==true) {
-					
-					rename($_SESSION["form"]["imageFile"], $_SESSION["form"]["imageDir"].$file_name);
-					$_SESSION["form"]["imageFile"] = $_SESSION["form"]["imageDir"].$file_name;
-					
-					$keyname = "profilePicture/".$file_name;
+
+				////////////////
+
+				if ($_FILES['avatar']['type'] == 'image/gif') {
+					$file_name = "A" . $_SESSION['user']['id'] . ".gif";
+					$type = "image/gif";
+				}
+				;
+				if ($_FILES['avatar']['type'] == 'image/png') {
+					$file_name = "A" . $_SESSION['user']['id'] . ".png";
+					$type = "image/png";
+				}
+				;
+				if ($_FILES['avatar']['type'] == 'image/jpeg') {
+					$file_name = "A" . $_SESSION['user']['id'] . ".jpeg";
+					$type = "image/jpeg";
+				}
+				;
+
+				$_SESSION["form"]["imageFile"] = $_SESSION["form"]["imageDir"] . $_SESSION["form"]["imageFile"];
+				mkdir($_SESSION["form"]["imageDir"], 0777);
+
+				$flagUploaded = false;
+
+				$max_ancho = 800;
+				$max_alto = 600;
+
+				$medidasimagen = getimagesize($_FILES['avatar']['tmp_name']);
+
+
+				//Si las imagenes tienen una resoluciÃ³n y un peso aceptable se suben tal cual
+				if ($medidasimagen[0] < 1280 && $_FILES['avatar']['size'] < 1000000) {
+
+					$nombrearchivo = $_FILES['avatar']['name'];
+					move_uploaded_file($_FILES["avatar"]["tmp_name"], $_SESSION["form"]["imageFile"]);
+					$flagUploaded = true;
+				}
+
+
+				//Si no, se generan nuevas imagenes optimizadas
+				else {
+					$nombrearchivo = $_FILES['avatar']['name'];
+
+					//Redimensionar
+					$rtOriginal = $_FILES['avatar']['tmp_name'];
+
+					if ($_FILES['avatar']['type'] == 'image/jpeg') {
+						$original = imagecreatefromjpeg($rtOriginal);
+					} else if ($_FILES['avatar']['type'] == 'image/png') {
+						$original = imagecreatefrompng($rtOriginal);
+					} else if ($_FILES['avatar']['type'] == 'image/gif') {
+						$original = imagecreatefromgif($rtOriginal);
+					}
+
+					list($ancho, $alto) = getimagesize($rtOriginal);
+
+					$x_ratio = $max_ancho / $ancho;
+					$y_ratio = $max_alto / $alto;
+
+					if (($ancho <= $max_ancho) && ($alto <= $max_alto)) {
+						$ancho_final = $ancho;
+						$alto_final = $alto;
+					} elseif (($x_ratio * $alto) < $max_alto) {
+						$alto_final = ceil($x_ratio * $alto);
+						$ancho_final = $max_ancho;
+					} else {
+						$ancho_final = ceil($y_ratio * $ancho);
+						$alto_final = $max_alto;
+					}
+
+					$lienzo = imagecreatetruecolor($ancho_final, $alto_final);
+
+					imagecopyresampled($lienzo, $original, 0, 0, 0, 0, $ancho_final, $alto_final, $ancho, $alto);
+
+					//imagedestroy($original);
+
+					if ($_FILES['avatar']['type'] == 'image/jpeg') {
+						imagejpeg($lienzo, $_SESSION["form"]["imageFile"]);
+						$flagUploaded = true;
+					} else if ($_FILES['avatar']['type'] == 'image/png') {
+						imagepng($lienzo, $_SESSION["form"]["imageFile"]);
+						$flagUploaded = true;
+					} else if ($_FILES['avatar']['type'] == 'image/gif') {
+						imagegif($lienzo, $_SESSION["form"]["imageFile"]);
+						$flagUploaded = true;
+					}
+
+
+				}
+				////////
+
+
+
+				if ($flagUploaded == true) {
+
+					rename($_SESSION["form"]["imageFile"], $_SESSION["form"]["imageDir"] . $file_name);
+					$_SESSION["form"]["imageFile"] = $_SESSION["form"]["imageDir"] . $file_name;
+
+					$keyname = "profilePicture/" . $file_name;
 					$dataImg = $_SESSION["form"]["imageFile"];
-					
+
 					try {
 						// Upload data.
 						$result = $s3->putObject([
@@ -173,85 +165,52 @@ if ( $stmt ) {
 							'StorageClass' => 'REDUCED_REDUNDANCY'
 						]);
 
-					
+
 						// Print the URL to the object.
 						// echo $result['ObjectURL'] . PHP_EOL;
-						$urlImage = "https://asiaorientalbucket.s3.sa-east-1.amazonaws.com/profilePicture/".$file_name;
+						$urlImage = "https://asiaorientalbucket.s3.sa-east-1.amazonaws.com/profilePicture/" . $file_name;
 					} catch (S3Exception $error) {
 						echo $error->getMessage() . PHP_EOL;
 					}
 
-					$sql3 = "UPDATE users SET avatar=N'".$urlImage."' WHERE id=".$_SESSION['user']['id'].";";  
-					$stmt3 = mysqli_query( $conn, $sql3);  
-					
-					if ( $stmt3 ) {
-					
-					$_SESSION['user']['avatar'] = $urlImage;
-					$_SESSION['notification']['type']="success";
-					$_SESSION['notification']['message']=$_SESSION['language']['Your profile is updated properly'];
-					$_SESSION['userLog']['description']="The user profile is updated properly.";
-					
-					} else {
-					$_SESSION['notification']['type']="warning";
-					$_SESSION['notification']['message']=$_SESSION['language']['Your profile is updated properly'];
-					$_SESSION['userLog']['description']="The user profile is updated without avatar.";
-					} 
-				} else {
-					$_SESSION['notification']['type']="warning";
-					$_SESSION['notification']['message']=$_SESSION['language']['Your profile is updated properly'];
-					$_SESSION['userLog']['description']="The user profile is updated without avatar.";
-				}
-				
-		}
-		
+					$sql3 = "UPDATE users SET avatar=N'" . $urlImage . "' WHERE id=" . $_SESSION['user']['id'] . ";";
+					$stmt3 = mysqli_query($conn, $sql3);
 
+					if ($stmt3) {
+
+						$_SESSION['user']['avatar'] = $urlImage;
+						$_SESSION['notification']['type'] = "success";
+						$_SESSION['notification']['message'] = $_SESSION['language']['Your profile is updated properly'];
+						$_SESSION['userLog']['description'] = "The user profile is updated properly.";
+
+					} else {
+						$_SESSION['notification']['type'] = "warning";
+						$_SESSION['notification']['message'] = $_SESSION['language']['Your profile is updated properly'];
+						$_SESSION['userLog']['description'] = "The user profile is updated without avatar.";
+					}
+				} else {
+					$_SESSION['notification']['type'] = "warning";
+					$_SESSION['notification']['message'] = $_SESSION['language']['Your profile is updated properly'];
+					$_SESSION['userLog']['description'] = "The user profile is updated without avatar.";
+				}
+
+			}
+
+
+		}
+
+
+
+	} else {
+		$_SESSION['notification']['type'] = "error";
+		$_SESSION['notification']['message'] = "Your profile is not updated properly, please try again or contact the Administrator.";
+		$_SESSION['userLog']['description'] = "The user profile is not updated properly, please try again or contact the Administrator.";
 	}
 
-
-	
-}
-else   
-{  
-		$_SESSION['notification']['type']="error";
-		$_SESSION['notification']['message']="Your profile is not updated properly, please try again or contact the Administrator.";	
-		$_SESSION['userLog']['description']="The user profile is not updated properly, please try again or contact the Administrator.";	
-}  
-
-header ("Location: userProfile.php?formStatus=view");
-ob_end_flush();
-mysqli_free_result( $stmt);  
-mysqli_close( $conn);  
+	header("Location: userProfile.php?formStatus=view");
+	ob_end_flush();
+	mysqli_free_result($stmt);
+	mysqli_close($conn);
 }
 
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
